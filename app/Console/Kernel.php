bec8fa506977da5f445168c6a154adca72745cde
Gelo Lavadia <gelojoshualavadia@gmail.com>
2021-03-13 02:35:19 +0800

    Cron Job Test 2:45

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('weekly:kumusta')->weeklyOn(6, '01:45');
+        $schedule->command('weekly:kumusta')->weeklyOn(6, '02:45');
     }
 
     /**
