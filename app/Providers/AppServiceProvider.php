52387d4ee028e8168951b214fd0f112ce756560c
Gelo Lavadia <gelojoshualavadia@gmail.com>
2021-03-12 19:17:06 +0800

    Relocate Public Path 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind('path.public', function() {
-            return real_path(__DIR__);
+            return realpath(__DIR__);
         });
     }
 
